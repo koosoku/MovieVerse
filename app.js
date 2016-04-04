e0ca6a38ace2de046a9a8e15d69a01e822f8c07e
Kyle <kylekung@hotmail.com>
2016-04-04 18:54:11 -0400

    callback to avoid race conditions

--- app.js
+++ app.js
@@ -13,15 +13,7 @@ var posterSize = [
 // app.use(bodyParser());
 
 
-var movieList = [
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'},
-	{id: 1, desc: 'Movie', imagePath:'./images/movie.jpg'}
-];
+var movieList=[];
 
 function test(){
 	request('https://api.themoviedb.org/3/movie/550?api_key=8a439f408d3ed4c974abe73cc1645699', function (err, res, body) {
@@ -33,28 +25,38 @@ function test(){
 	});
 }
 
-function fetchMovieByID(ID) {
+function fetchMovieByID(ID, callback) {
 	request('https://api.themoviedb.org/3/movie/'+ID +'?api_key=8a439f408d3ed4c974abe73cc1645699',function(err,res,body){
 		// console.log(body);
 		if (!err && res.statusCode == 200) {
 			var obj = JSON.parse(body);
 			if(obj.poster_path){
 		    	console.log(baseURL + posterSize[5]+ obj.poster_path);
-		    	movieList.push({id: 1, desc: obj.original_title, imagePath: baseURL + posterSize[5]+ obj.poster_path});
+		    	callback({id: 1, desc: obj.original_title, imagePath: baseURL + posterSize[5]+ obj.poster_path});
 			}
+		}else{
+			console.log(err);
+			console.log(body);
 		}
 	});
 }
 
 function search(query){
+
 	request('https://api.themoviedb.org/3/search/keyword?api_key=8a439f408d3ed4c974abe73cc1645699&query='+query,function(err,res,body){
+		movieList=[];
 		if (!err && res.statusCode == 200) {
 			var jsonObj = JSON.parse(body);
 			for (var i = 0; i<jsonObj.results.length; i++) {
 			    console.log(jsonObj.results[i].id);
-			    fetchMovieByID(jsonObj.results[i].id);
+			    fetchMovieByID(jsonObj.results[i].id, function(result) {
+			    	movieList.push(result);
+			    });
 			}
 		console.log(jsonObj);
+		}else{
+			console.log(err);
+			console.log(body);
 		}
 
 	});
